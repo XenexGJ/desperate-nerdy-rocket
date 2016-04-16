c207bb01c947f5d038df0dc481130773b83def86
Christoph Heinen <heinen@fachschaft.info>
2016-04-16 03:47:20 +0200

    more upgrades

--- src/rocket.cpp
+++ src/rocket.cpp
@@ -21,7 +21,23 @@ Rocket::Rocket(std::vector<Upgrade*> upgradeList)
 	{
     	switch ((*it)->type)
     	{
+    		case UPGRADE_BODY:
+    			rocketbooster = &**it;
+    			std::cout << "it\n";
+    		break;
+    		case UPGRADE_GOGGLES:
+    			rocketbooster = &**it;
+    			std::cout << "it\n";
+    		break;
+    		case UPGRADE_WINGS:
+    			rocketbooster = &**it;
+    			std::cout << "it\n";
+    		break;
     		case UPGRADE_BOOSTER:
+    			rocketbooster = &**it;
+    			std::cout << "it\n";
+    		break;
+    		case UPGRADE_TAIL:
     			rocketbooster = &**it;
     			std::cout << "it\n";
     		break;
@@ -36,10 +52,30 @@ void Rocket::draw(sf::RenderTarget& target,sf::RenderStates states)const
 	sprite_rocket.setTexture(texture);
 	sprite_rocket.setPosition(position);
 	target.draw(sprite_rocket,states);
+	if(rocketbody != NULL)
+	{
+		rocketbody->setPosition(position);
+		target.draw(*rocketbody,states);
+	}
+	if(rocketgoggles != NULL)
+	{
+		rocketgoggles->setPosition(position);
+		target.draw(*rocketgoggles,states);
+	}
+	if(rocketwings != NULL)
+	{
+		rocketwings->setPosition(position);
+		target.draw(*rocketwings,states);
+	}
 	if(rocketbooster != NULL)
 	{
 		rocketbooster->setPosition(position);
 		target.draw(*rocketbooster,states);
+	}
+	if(rockettail != NULL)
+	{
+		rockettail->setPosition(position);
+		target.draw(*rockettail,states);
 	}
 }
 
